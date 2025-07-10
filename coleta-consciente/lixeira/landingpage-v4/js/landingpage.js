5d3a1364e0fa322d60debb3fc718a791743cfea6
alexritzmann <alexritzmann@gmail.com>
2025-07-10 17:02:01 -0300

    arquivos lixieira e temporários excluidos

--- coleta-consciente/lixeira/landingpage-v4/js/landingpage.js
+++
@@ -1,4 +0,0 @@
-window.addEventListener('scroll', function () {
-  var header = document.querySelector('header');
-  var scrolled = window.scrollY > 50;
-  header.classList.toggle('scrolled', scrolled);});
